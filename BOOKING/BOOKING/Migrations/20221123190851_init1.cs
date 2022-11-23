5cdd0331c11a37438af70024d97c5cf195c8a88d
msw022 <yung.drgz@tlen.pl>
2022-11-23 23:29:03 +0100

    fix

---
+++ BOOKING/BOOKING/Migrations/20221123190851_init1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BOOKING.Migrations
+{
+    public partial class init1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Locality",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Locality",
+                table: "Products");
+        }
+    }
+}
